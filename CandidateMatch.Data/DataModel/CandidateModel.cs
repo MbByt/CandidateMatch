7df7ba4c8c8ee1aebf6d6eff960ea1d632687e7c
Milad Bayat <Milad.Bayat@journeybeyond.com>
2021-03-08 23:45:25 +1100

    AddingJobAdderModels

---
+++ CandidateMatch.Data/DataModel/CandidateModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CandidateMatch.Data.Models
+{
+    public class CandidateModel
+    {
+        public int candidateId { set; get; }
+
+        public string name { set; get; }
+
+        public string skillTags { set; get; }
+
+        public List<string> skillTagsList
+        {
+            get
+            {
+                return skillTags.Split(",").ToList();
+            }
+        }
+    }
+}
---
+++ CandidateMatch.Data/DataModel/JobModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CandidateMatch.Data.Models
+{
+    public class JobModel
+    {
+        public int jobId { set; get; }
+
+        public string name { set; get; }
+
+        public string compny { set; get; }
+
+        public string skills { set; get; }
+
+        public List<string> skillsList
+        {
+            get
+            {
+                return skills.Split(",").ToList();
+            }
+        }
+    }
+}
