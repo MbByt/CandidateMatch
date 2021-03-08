46163fa7687ac9327e4db63ad80f2ecd4e0fa6a9
Milad Bayat <Milad.Bayat@journeybeyond.com>
2021-03-09 00:16:08 +1100

    AddingCandidateMatchService

---
+++ CandidateMatch.Services/CandidateMatcher.cs
@@ -0,0 +1,66 @@
+﻿using CandidateMatch.Data.Context;
+using CandidateMatch.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CandidateMatch.Services
+{
+    public class CandidateMatcher : ICandidateMatcher
+    {
+        private readonly ICandidateAPIContext _candidateAPIContext;
+        private readonly IJobAPIContext _jobAPIContext;
+        public CandidateMatcher(ICandidateAPIContext candidateAPIContext, IJobAPIContext jobAPIContext)
+        {
+            _candidateAPIContext = candidateAPIContext;
+           _jobAPIContext = jobAPIContext;
+        }
+        public async Task<CandidateModel> GetBestMatchedCandidateAsync( int JobId)
+        {
+            List<CandidateModel> CandidateList = await _candidateAPIContext.GetAsync<List<CandidateModel>>();
+            List<JobModel> JobList = await _jobAPIContext.GetAsync<List<JobModel>>();
+
+            List<(int CandidateID, int Points)> Points = new List<(int CandidateID, int Points)>();
+
+            var Job = JobList?.Where(x => x.jobId == JobId).FirstOrDefault();
+            if (Job is null) return null;
+
+          
+            int CandidatePoints = 0;
+            foreach (var candidate in CandidateList)
+             {
+                int CandidateSkillsCount = candidate.skillTagsList.Count;
+                int JobSkillsCount = Job.skillsList.Count;
+
+                foreach (string CandidateSkill in candidate.skillTagsList)
+                {
+                    
+                    foreach (string JobSkill in Job.skillsList)
+                    {
+
+                        if( CandidateSkill.Trim().ToUpper() == JobSkill.Trim().ToUpper())
+                        {
+                            CandidatePoints += JobSkillsCount * CandidateSkillsCount;
+
+                        }
+                        JobSkillsCount -= 1;
+                    }
+                    CandidateSkillsCount -= 1;
+                }
+
+                Points.Add((candidate.candidateId, CandidatePoints));
+            }
+
+
+            int MaxPointOfCandidates = Points.Max(c => c.Points);
+
+            var BestCandidateMatch = Points.Where(c => c.Points == MaxPointOfCandidates).FirstOrDefault();
+
+            var BestCandidateDetails =  CandidateList.Where(c =>c.candidateId == BestCandidateMatch.CandidateID).FirstOrDefault();
+
+            return BestCandidateDetails;
+        }
+    }
+
+}
---
+++ CandidateMatch.Services/ICandidateMatcher.cs
@@ -0,0 +1,15 @@
+﻿using CandidateMatch.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CandidateMatch.Services
+{
+    public interface ICandidateMatcher
+    {
+
+        Task< CandidateModel> GetBestMatchedCandidateAsync(int JobId);
+
+   }
+}
