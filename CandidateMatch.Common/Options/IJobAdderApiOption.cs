043cbc027588729e1246f656f21eb965690982ff
Milad Bayat <Milad.Bayat@journeybeyond.com>
2021-03-08 23:41:50 +1100

    Adding JobAdderApiOptions

---
+++ CandidateMatch.Common/Options/IJobAdderApiOption.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CandidateMatch.Common.Options
+{
+    public interface IJobAdderApiOptions
+    {
+        string JobApiEndPoint { get; set; }
+
+        string CandidateApiEndPoint { get; set; }
+
+        string CandidatesAPIPath { get; set; }
+
+        string JobsAPIPath { get; set; }
+    }
+    
+}
---
+++ CandidateMatch.Common/Options/JobAdderApiOptions.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace CandidateMatch.Common.Options
+{
+    public  class JobAdderApiOptions :IJobAdderApiOptions
+    {
+        private readonly IConfiguration _configuration;
+
+        public JobAdderApiOptions(IConfiguration Configuration)
+        {
+            _configuration = Configuration;
+            JobApiEndPoint = _configuration["JobApiEndPoint"];
+            CandidateApiEndPoint = _configuration["CandidateApiEndPoint"];
+            CandidatesAPIPath = _configuration["CandidatesAPIPath"];
+            JobsAPIPath = _configuration["JobsAPIPath"];
+        }
+
+        public  string JobApiEndPoint { get; set; }
+        public  string CandidateApiEndPoint { get; set; } 
+        public  string CandidatesAPIPath { get; set; } 
+        public  string JobsAPIPath { get; set; } 
+ 
+    }
+}
